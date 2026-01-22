6236c4f7a79ec8adbb99bccb6ea97f0e2a4df2aa
AlvaroEP <alvaroestevezpazos@gmail.com>
2026-01-22 11:10:14 +0100

    NavBar con cuadro de busqueda

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <!-- contenedor padre a 80% y centrado -->
-  <div id="app">
-    <NavBar /> <!-- Fijo arriba, pero dentro del 80% -->
-    <main class="flex-grow-1 overflow-auto">
-      <router-view />
-    </main>
-    <FooTer /> <!-- Fijo abajo, pero dentro del 80% -->
+  <div>
+    <NavBar />
+    <div id="app" class="app-container">
+      <main class="flex-grow-1 overflow-auto">
+        <router-view />
+      </main>
+    </div>
+    <FooTer />
   </div>
 </template>
 
@@ -24,25 +26,24 @@ onMounted(() => {
 </script>
 <!--Start of Tawk.to Script-->
 <script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/69709eabcba9c0197c1235ff/1jffunk34';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function () {
+  var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+  s1.async = true;
+  s1.src = 'https://embed.tawk.to/69709eabcba9c0197c1235ff/1jffunk34';
+  s1.charset = 'UTF-8';
+  s1.setAttribute('crossorigin', '*');
+  s0.parentNode.insertBefore(s1, s0);
 })();
 </script>
 <!--End of Tawk.to Script-->
 
 <style scoped>
-#app {
+.app-container {
   max-width: 80%;
   margin: 0 auto;
   /* centra horizontalmente */
-  height: 100vh;
-  /* equivalente a vh-100 */
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
 }
--- src/components/common/NavBar.vue
+++ src/components/common/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
-  <nav class="navbar navbar-dark bg-primary sticky-top navbar-expand-lg" ref="navbarRef">
+  <nav class="navbar navbar-dark bg-primary navbar-expand-lg" ref="navbarRef">
     <div class="container-fluid d-flex align-items-center position-relative">
-      <!-- Logo y hamburguesa a la izquierda en móvil -->
+
       <div class="d-flex align-items-center flex-shrink-0">
         <a class="navbar-brand me-2" href="#">EmpresaTeis</a>
         <button class="navbar-toggler d-lg-none" type="button" @click="toggleMenu" aria-controls="navbarNav"
@@ -10,7 +10,53 @@
         </button>
       </div>
 
-      <div class="dropdown">
+      <div class="navbar-collapse flex-grow-1 ps-5 pe-3 ps-lg-0 pe-lg-0"
+        :class="{ 'menu-abierto': menuAbierto, 'menu-cerrado': !menuAbierto }" id="navbarNav" ref="navbarCollapse">
+        <ul class="navbar-nav mx-lg-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/" @click="cerrarMenu">
+              <span class="nav-underline">Inicio</span>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/clientes" @click="cerrarMenu">
+              <span class="nav-underline">Clientes</span>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/noticias" @click="cerrarMenu">
+              <span class="nav-underline">Noticias</span>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/ventas" @click="cerrarMenu">
+              <span class="nav-underline">Ventas</span>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/modelos" @click="cerrarMenu">
+              <span class="nav-underline">Modelos</span>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/taller" @click="cerrarMenu">
+              <span class="nav-underline">Taller</span>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/contacto" @click="cerrarMenu">
+              <span class="nav-underline">Contacto</span>
+            </router-link>
+          </li>
+          <form class="d-flex pb-3 ps-0 pb-lg-0 form-control-sm" role="search">
+            <input class="form-control me-2" type="search" placeholder="Buscar..." aria-label="Buscar"
+              v-model="query" />
+            <button class="btn btn-light" type="submit"><i class="bi bi-search"></i></button>
+          </form>
+        </ul>
+      </div>
+
+      <div class="dropdown" ref="dropdownRef">
         <button class="btn btn-primary dropdown-toggle" type="button" @click="toggleDropdown">
           <i class="bi bi-person fs-2"></i>
         </button>
@@ -29,33 +75,7 @@
           </li>
         </ul>
       </div>
-      
-      <div class="navbar-collapse flex-grow-1" :class="{ 'menu-abierto': menuAbierto, 'menu-cerrado': !menuAbierto }"
-        id="navbarNav" ref="navbarCollapse">
-        <ul class="navbar-nav mx-lg-auto">
-          <li class="nav-item">
-            <router-link class="nav-link" to="/" @click="cerrarMenu">Inicio</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/clientes" @click="cerrarMenu">Clientes</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/noticias" @click="cerrarMenu">Noticias</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/ventas" @click="cerrarMenu">Ventas</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/modelos" @click="cerrarMenu">Modelos</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/taller" @click="cerrarMenu">Citas Taller</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/contacto" @click="cerrarMenu">Contacto</router-link>
-          </li>
-        </ul>
-      </div>
+
     </div>
   </nav>
 </template>
@@ -113,6 +133,7 @@ const cerrarSesion = async () => {
   cerrarTodo()
   logout()
   setTimeout(() => {
+    Talle
     window.location.href = '/'
   }, 1500)
 }
@@ -158,5 +179,68 @@ const cerrarSesion = async () => {
   .dropdown.ms-lg-auto {
     margin-left: auto !important;
   }
+}
+
+/* En pantallas pequeñas, evitar que el colapso ocupe ancho y empuje el icono */
+@media (max-width: 991.98px) {
+  .container-fluid {
+    position: relative;
+    flex-wrap: nowrap;
+    align-items: center;
+  }
+
+  /* Hacer que el collapse sea overlay (no ocupa ancho en la fila) */
+  .navbar-collapse {
+    position: absolute;
+    top: 100%;
+    left: 0;
+    right: 0;
+    width: 100%;
+    flex: 0 0 auto !important;
+    max-height: 0;
+    overflow: hidden;
+    background-color: var(--bs-primary, #0d6efd);
+    transition: max-height 0.35s ease;
+    z-index: 1040;
+  }
+
+  .navbar-collapse.menu-abierto {
+    max-height: 480px;
+  }
+
+  .dropdown {
+    margin-left: auto;
+    z-index: 1100;
+  }
+}
+
+/* Mantener color y la variante usada (.underline-3-inner) */
+.nav-link {
+  color: rgba(255, 255, 255, 0.95);
+}
+
+.nav-underline {
+  display: inline-block;
+  background-image: linear-gradient(#fff, #fff);
+  background-repeat: no-repeat;
+  background-size: 0% 2px;
+  background-position: center 100%;
+  transition: background-size .28s ease;
+  padding: 0;
+}
+
+.nav-underline:hover,
+/* activar cuando el padre recibe hover/focus/active o está marcado como router-link-active */
+.nav-link:hover .nav-underline,
+.nav-link:focus .nav-underline,
+.nav-link:active .nav-underline,
+.nav-link.router-link-active .nav-underline,
+.nav-link.active .nav-underline {
+  background-size: 80% 2px;
+}
+
+/* pequeña separación para el menú colapsado */
+.navbar-collapse .nav-item {
+  margin-bottom: .25rem;
 }
 </style>
